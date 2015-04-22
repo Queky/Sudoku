8126559749ac4632a450801a1a694afba125e200
eko44 <eko44ahedo@gmail.com>
2015-04-22 10:08:58 +0200

    añadir cosas

--- src/main/java/Ventana/MenuSudoku.java
+++ src/main/java/Ventana/MenuSudoku.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Color;
 
 public class MenuSudoku extends JFrame {
 
@@ -31,12 +34,15 @@ public class MenuSudoku extends JFrame {
 	 * Create the frame.
 	 */
 	public MenuSudoku() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		initialize();
 	}
-
+	private void initialize() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 330, 390);
+		contentPane = new JPanel();
+		contentPane.setForeground(Color.RED);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
 }
