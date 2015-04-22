de50ec588634887b28c991f30b82c6e927e5e0cb
eko44 <eko44ahedo@gmail.com>
2015-04-22 10:44:39 +0200

    Cambios

--- src/main/java/Ventana/LoginSudoku.java
+++ src/main/java/Ventana/LoginSudoku.java
@@ -122,7 +122,7 @@ public class LoginSudoku extends JFrame {
 								con=DriverManager.getConnection(url);
 								System.out.println("conexion abierta");
 								Statement st= con.createStatement();
-								st.executeUpdate("INSERT INTO `sudoku`.`infojuego` (`nombreJugador`,`nivel_dificultad`) VALUES ('"+textField.getText()+"','"+comboBox.getSelectedIndex()+"');");
+								st.executeUpdate("INSERT INTO `sudoku`.`infojuego` (`nombreJugador`,`nivel_dificultad`) VALUES ('"+textField.getText()+"','"+(comboBox.getSelectedIndex()+1)+"');");
 								con.close();
 								
 								}catch(SQLException e5){System.out.println(e5.toString()); }
