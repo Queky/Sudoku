fbf561ee1301d4928255f5ed0f79d026f5239735
eko44 <eko44ahedo@gmail.com>
2015-04-23 21:37:20 +0200

    Cambios Intentos de imagen de fondo

--- src/main/java/Ventana/MenuSudoku.java
+++ src/main/java/Ventana/MenuSudoku.java
@@ -1,17 +1,29 @@
 package Ventana;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+
 import java.awt.Color;
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class MenuSudoku extends JFrame {
-
+	public Image imagenFondo;
+	public URL fondo;
 	private JPanel contentPane;
 
 	/**
@@ -29,6 +41,7 @@ public class MenuSudoku extends JFrame {
 			}
 		});
 	}
+	
 
 	/**
 	 * Create the frame.
@@ -36,13 +49,22 @@ public class MenuSudoku extends JFrame {
 	public MenuSudoku() {
 		initialize();
 	}
+	
 	private void initialize() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 330, 390);
-		contentPane = new JPanel();
-		contentPane.setForeground(Color.RED);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		String directory = Paths.get(".").toAbsolutePath().normalize().toString();
+		fondo = this.getClass().getResource(directory+"/Imagenes/Fondo-Web-d-Madera-rojo-oscuro.jpg");
+		imagenFondo = new ImageIcon(fondo).getImage();
+		Container contenedor = getContentPane();
+		contenedor.add(panel);
+		
+		//add(new JLabel(new ImageIcon(directory+"/Imagenes/Fondo-Web-d-Madera-rojo-oscuro.jpg")));
 	}
+	public JPanel panel = new JPanel(){
+	public void paitComponent(Graphics g){
+		g.drawImage(imagenFondo,0,0,getWidth(),getHeight(),this);
+		
+	}
+	};
 }
