6a8ba7eb3babf2e3f0c83e9240ca2a9764de7c81
eko44 <eko44ahedo@gmail.com>
2015-04-24 13:48:45 +0200

    AñadirBoton y cerrar con el

--- src/main/java/Ventana/MenuSudoku.java
+++ src/main/java/Ventana/MenuSudoku.java
@@ -8,37 +8,51 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class MenuSudoku extends JFrame {
-	private JButton btnNewButton;
-public static void main(String[] args) {
-EventQueue.invokeLater(new Runnable() {
-public void run() {
-try {
-MenuSudoku frame = new MenuSudoku();
-frame.setVisible(true);
-} catch (Exception e) {
-e.printStackTrace();
-}
-}
-});
-}
-public MenuSudoku() {
-	initialize();
-}
-private void initialize() {
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setBounds(100, 100, 450, 450);
-	PanelImagen p = new PanelImagen();
-	p.setBorder(new EmptyBorder(5, 5, 5, 5));
-	setContentPane(p);
-	p.setLayout(null);
-	p.add(getBtnNewButton());
-}
-	private JButton getBtnNewButton() {
-		if (btnNewButton == null) {
-			btnNewButton = new JButton("New button");
-			btnNewButton.setBounds(190, 100, 120, 30);
+	private JButton btnNuevoJuego;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuSudoku frame = new MenuSudoku();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public MenuSudoku() {
+		initialize();
+	}
+
+	private void initialize() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 450);
+		PanelImagen p = new PanelImagen();
+		p.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(p);
+		p.setLayout(null);
+		p.add(getBtnNuevoJuego());
+	}
+
+	private JButton getBtnNuevoJuego() {
+		if (btnNuevoJuego == null) {
+			btnNuevoJuego = new JButton("NuevoJuego");
+			btnNuevoJuego.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					setVisible(false);
+					dispose();
+
+				}
+			});
+			btnNuevoJuego.setBounds(165, 165, 120, 30);
 		}
-		return btnNewButton;
+		return btnNuevoJuego;
 	}
 }
