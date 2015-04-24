466017dc756981e3b7cd27a35af16181fc68e724
eko44 <eko44ahedo@gmail.com>
2015-04-24 12:10:23 +0200

    Añadir botones

--- src/main/java/Ventana/MenuSudoku.java
+++ src/main/java/Ventana/MenuSudoku.java
@@ -5,7 +5,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 public class MenuSudoku extends JFrame {
+	private JButton btnNewButton;
 public static void main(String[] args) {
 EventQueue.invokeLater(new Runnable() {
 public void run() {
@@ -19,11 +23,22 @@ e.printStackTrace();
 });
 }
 public MenuSudoku() {
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-setBounds(100, 100, 450, 300);
-PanelImagen p = new PanelImagen();
-p.setBorder(new EmptyBorder(5, 5, 5, 5));
-p.setLayout(new BorderLayout(0, 0));
-setContentPane(p);
+	initialize();
 }
+private void initialize() {
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setBounds(100, 100, 450, 450);
+	PanelImagen p = new PanelImagen();
+	p.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(p);
+	p.setLayout(null);
+	p.add(getBtnNewButton());
+}
+	private JButton getBtnNewButton() {
+		if (btnNewButton == null) {
+			btnNewButton = new JButton("New button");
+			btnNewButton.setBounds(190, 100, 120, 30);
+		}
+		return btnNewButton;
+	}
 }
