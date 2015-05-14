b6281c047d51adc3fd794398a50d92f9ede07806
eko44 <eko44ahedo@gmail.com>
2015-05-14 18:10:07 +0200

    Cambios

---
+++ src/test/java/ModeloTest/CalcularPuntuacionTest.java
@@ -0,0 +1,26 @@
+package ModeloTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+import Modelo.CalcularPuntuacion;
+import Modelo.Jugador;
+
+public class CalcularPuntuacionTest {
+	CalcularPuntuacion cP1;
+	@Before
+	public void setUp() throws Exception {
+		cP1 = new CalcularPuntuacion();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	@Test
+	public void testCalcularPuntuacion(){
+		
+	}
+}
