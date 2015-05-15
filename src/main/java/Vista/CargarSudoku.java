7a8b933c24b568d39896d364c42987ced99dea27
Queky <inakisanchez_92@hotmail.com>
2015-05-15 13:23:28 +0200

    cambios

--- src/main/java/Vista/CargarSudoku.java
+++ src/main/java/Vista/CargarSudoku.java
@@ -3,7 +3,6 @@ package Vista;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.RenderingHints.Key;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
