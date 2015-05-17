42ff18bef9b8e9293c4e92dbda01a1beb78d6592
eko44 <eko44ahedo@gmail.com>
2015-05-17 16:28:35 +0200

    correcciones

--- src/main/java/Modelo/ConexionConBBDD.java
+++ src/main/java/Modelo/ConexionConBBDD.java
@@ -79,7 +79,7 @@ public class ConexionConBBDD {
 			 return resultado;
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return "No tienes puntuación";		
+			return null;		
 		}
 	}
 	public void anyadirPuntuacionJuego(String nombreJugador , String codigoSudoku, int puntuacion){
