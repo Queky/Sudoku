ec3d45fde7d5958d10797be748c23eee20d7b42c
eko44 <eko44ahedo@gmail.com>
2015-05-17 15:59:29 +0200

    BBDD

--- BBDD/sudokuBBDD.sql
+++ BBDD/sudokuBBDD.sql
@@ -16,25 +16,52 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `jugadores`
+-- Table structure for table `infojugador`
 --
 
-DROP TABLE IF EXISTS `jugadores`;
+DROP TABLE IF EXISTS `infojugador`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `jugadores` (
+CREATE TABLE `infojugador` (
+  `nombreJugador` varchar(15) NOT NULL,
+  `cod_sudoku` varchar(10) DEFAULT NULL,
+  `puntuacion` int(11) NOT NULL,
+  KEY `nombreJugador` (`nombreJugador`),
+  CONSTRAINT `nombreJugador` FOREIGN KEY (`nombreJugador`) REFERENCES `jugador` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `infojugador`
+--
+
+LOCK TABLES `infojugador` WRITE;
+/*!40000 ALTER TABLE `infojugador` DISABLE KEYS */;
+INSERT INTO `infojugador` VALUES ('Eneko','s004',2971);
+/*!40000 ALTER TABLE `infojugador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jugador`
+--
+
+DROP TABLE IF EXISTS `jugador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jugador` (
   `nombre` varchar(15) NOT NULL,
   PRIMARY KEY (`nombre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `jugadores`
+-- Dumping data for table `jugador`
 --
 
-LOCK TABLES `jugadores` WRITE;
-/*!40000 ALTER TABLE `jugadores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `jugadores` ENABLE KEYS */;
+LOCK TABLES `jugador` WRITE;
+/*!40000 ALTER TABLE `jugador` DISABLE KEYS */;
+INSERT INTO `jugador` VALUES ('Eneko');
+/*!40000 ALTER TABLE `jugador` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -46,4 +73,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-04-15  9:39:30
+-- Dump completed on 2015-05-17 15:58:59
